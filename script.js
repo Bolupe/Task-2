838644f3f369392abe37e7d89ee7dd3d9c0f045c
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-09-07 19:37:18 +0100

    corrected code for to utc time

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 // script.js
-function updateNigerianTime() {
+function updateUTCTime() {
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const nigeriaTimeOffset = 1; // UTC+1 for Nigerian Time
     const currentDate = new Date();
-    
-    // Adjust the time by adding the offset in hours
-    currentDate.setUTCHours(currentDate.getUTCHours() + nigeriaTimeOffset);
-    
     const currentDay = daysOfWeek[currentDate.getUTCDay()];
-    const currentNigerianTime = currentDate.toISOString().substr(11, 8);
+    const currentUTCTime = currentDate.toISOString().substr(11, 8);
 
     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Day of the Week: ${currentDay}`;
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = `UTC Time: ${currentNigerianTime}`;
+    document.querySelector('[data-testid="currentUTCTime"]').textContent = `UTC Time: ${currentUTCTime}`;
 }
 
-// Update Nigerian time every second
-setInterval(updateNigerianTime, 1000);
+// Update UTC time every second
+setInterval(updateUTCTime, 1000);
 
 // Initial update
-updateNigerianTime();
+updateUTCTime();
