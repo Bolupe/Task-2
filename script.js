f1e8568ba60c1e30799eb464d06911dcc74f60c3
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-09-08 20:33:32 +0100

    better styling

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-function updateUTCTime() {
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const currentDate = new Date();
-    const currentDay = daysOfWeek[currentDate.getUTCDay()];
-    const currentUTCTime = currentDate.toISOString().substr(11, 8);
+const desiredTime = new Date();
+desiredTime.setUTCHours(8);
+desiredTime.setUTCMinutes(15);
+desiredTime.setUTCSeconds(0);
 
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Day of the Week: ${currentDay}`;
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = `UTC Time: ${currentUTCTime}`;
-}
+const timestamp = desiredTime.getTime();
 
-// Update UTC time every second
-setInterval(updateUTCTime, 1000);
+const timeElement = document.getElementById("time");
+timeElement.innerHTML = timestamp.toString();
 
-updateUTCTime();
+console.log(timestamp);
+
+
+
+
+
+
+// function updateUTCTime() {
+//     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//     const currentDate = new Date();
+//     const currentDay = daysOfWeek[currentDate.getUTCDay()];
+//     const currentUTCTime = currentDate.toISOString().substr(11, 8);
+
+//     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Day of the Week: ${currentDay}`;
+//     document.querySelector('[data-testid="currentUTCTime"]').textContent = `UTC Time: ${currentUTCTime}`;
+// }
+
+// // Update UTC time every second
+// setInterval(updateUTCTime, 1000);
+
+// updateUTCTime();
