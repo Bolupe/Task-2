6f3ec70adbe5206948b5f27adbb4287c34c1ac88
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-09-08 22:58:55 +0100

    changes to UTC time to display in milliseconds

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-function updateDayAndTime() {
-    const currentDayOfTheWeekElement = document.getElementById("currentDayOfTheWeek");
-    const currentTimeElement = document.getElementById("time");
+const timeEl = document.getElementById("currentDayOfTheWeek")
+const dayEl = document.getElementById("time")
 
-    // Get the current UTC time
-    const currentUtcTime = new Date().toUTCString();
-
-    // Get the current day of the week
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const currentDayOfWeek = daysOfWeek[new Date().getUTCDay()];
-
-    // Update the elements with the current values
-    currentDayOfTheWeekElement.textContent = currentDayOfWeek;
-    currentTimeElement.textContent = currentUtcTime.slice(17, -7); // Extract and display only the time part
-}
-
-// Call the function to update the day and time initially
-updateDayAndTime();
-
-// Update the day and time every second
-setInterval(updateDayAndTime, 1000);
+const date = new Date() // create new Date Object 
+const currentDay = date.getDay() // get current day 
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // array list of days of the week 
+const nameOfCurrentDay = daysOfWeek[currentDay] // get the exact day from the "daysOFWeek" array 
+currentDayOfTheWeek.textContent = " " +   nameOfCurrentDay // display in html 
+const timeUTCInMilliseconds = date.getTime() // get time in milliseconds 
+time.textContent = " " +  timeUTCInMilliseconds
